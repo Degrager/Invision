2f56134aadab3992591095fdbcf05282d3ece718
Degrager <zrileys0@gmail.com>
2017-11-06 00:26:59 -0800

    Added Title to Prefs and Respring Button

--- prefs/ivsnRootListController.m
+++ prefs/ivsnRootListController.m
@@ -1,4 +1,65 @@
 #include "ivsnRootListController.h"
+#import <spawn.h>
+
+
+
+@protocol PreferencesTableCustomView
+- (id)initWithSpecifier:(id)arg1;
+
+@optional
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1;
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1 inTableView:(id)arg2;
+@end
+
+@interface ivsnCustomCell : UITableViewCell <PreferencesTableCustomView> {
+    UILabel *label;
+    UILabel *underLabel;
+}
+@end
+
+
+
+
+@implementation ivsnCustomCell
+
+- (id)initWithSpecifier:(PSSpecifier *)specifier
+{
+    self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    if (self) {
+
+	#define kWidth [[UIApplication sharedApplication] keyWindow].frame.size.width
+	CGRect labelFrame = CGRectMake(0, -15, kWidth, 70);
+      CGRect underLabelFrame = CGRectMake(0, 22, kWidth, 70);
+	
+label = [[UILabel alloc] initWithFrame:labelFrame];
+	[label setNumberOfLines:1];
+	label.font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:60];
+	[label setText:@"Invision"];
+	[label setBackgroundColor:[UIColor clearColor]];
+	label.textColor = [UIColor blackColor];
+	label.textAlignment = NSTextAlignmentCenter;
+
+underLabel = [[UILabel alloc] initWithFrame:underLabelFrame];
+	[underLabel setNumberOfLines:1];
+	underLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:12];
+	[underLabel setText:@"All-in-One Theming, Springboard, Widgets, and More Tweak"];
+	[underLabel setBackgroundColor:[UIColor clearColor]];
+	underLabel.textColor = [UIColor blackColor];
+	underLabel.textAlignment =
+NSTextAlignmentCenter;
+
+       [self addSubview:label];
+       [self addSubview:underLabel];
+    }
+    return self;
+}
+- (CGFloat)preferredHeightForWidth:(CGFloat)arg1 {
+    CGFloat prefHeight = 75.0;
+    return prefHeight;
+}
+@end
+
+
 
 @implementation ivsnRootListController
 
@@ -8,6 +69,27 @@
 	}
 
 	return _specifiers;
+}
+
+
+
+//Twitter Degrager
+
+-(void) Dtwitter
+	{	
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://twitter.com/Degrager_Dev"]];
+	}
+
+
+
+//Respring Button
+
+-(void) respring {
+pid_t pid;
+int status;
+const char* args[] = {"killall", "-9", "backboardd", NULL};
+posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+waitpid(pid, &status, WEXITED);
 }
 
 @end
