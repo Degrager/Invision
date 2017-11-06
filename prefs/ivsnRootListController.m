feff92e070cb02656b6e3c09bb4917904f21d94f
Degrager <zrileys0@gmail.com>
2017-11-06 00:12:26 -0800

    Added Basic Setup files for editing

---
+++ prefs/ivsnRootListController.m
@@ -0,0 +1,13 @@
+#include "ivsnRootListController.h"
+
+@implementation ivsnRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
+	}
+
+	return _specifiers;
+}
+
+@end
